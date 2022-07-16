e8bd123701fc5df1e004d5fbbdc63d038ec591f6
augustowhitaker <awhitakerflx@gmail.com>
2022-07-16 01:02:05 -0300

    terceira-parte

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,13 +2,14 @@ import SocialNetworks from './SocialNetworks';
 import React from 'react'
 import Avatar from '../img/fotoGuto.jpg';
 import '../styles/components/sidebar.sass';
+import InformationContainer from "./InformationContainer";
 
 const Sidebar = () => {
   return <aside id='sidebar'>
     <img src={Avatar} alt="Augusto Whitaker" />
     <p className='title'>Desenvolvedor</p>
     <SocialNetworks />
-    <p>informações de contato</p>
+    <InformationContainer />
     <a href="#" className='btn'>Download currículo</a>
   </aside>
 }
