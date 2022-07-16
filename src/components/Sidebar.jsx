f9f81eaf7f9439b5c7779453b3e20ca4826aeb6b
augustowhitaker <awhitakerflx@gmail.com>
2022-07-15 23:29:29 -0300

    Inicio_Portfolio

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,16 @@
+import SocialNetworks from './SocialNetworks';
+import React from 'react'
+import Avatar from '../img/fotoGuto.jpg';
+import '../styles/components/sidebar.sass';
+
+const Sidebar = () => {
+  return <aside id='sidebar'>
+    <img src={Avatar} alt="Augusto Whitaker" />
+    <p className='title'>Desenvolvedor</p>
+    <SocialNetworks />
+    <p>informações de contato</p>
+    <a href="" className='btn'>Download currículo</a>
+  </aside>
+}
+
+export default Sidebar
